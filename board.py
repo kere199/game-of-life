16e10232fe6f3fe917d169238742474ea0c53cc1
kere199 <kereleishviliirakli81@gmail.com>
2023-10-16 09:31:52 +0400

    add methods to the board class

--- board.py
+++ board.py
@@ -14,3 +14,38 @@ class Board():
     
     def place_cell(self,row,col):
         self.board[row][col] = True
+
+    def get_cell(self,row,col):
+        if 0 <= row < self.height and 0<=col<self.width:
+            return self.board[row][col]
+        return False 
+
+    def get_alive_neighbours(self,row, col):
+        accumulator = 0
+        for r in (-1,0,1):
+            for c in (-1,0,1):
+                if r == c and r == 0 :
+                    continue
+                if self.get_cell(row+r ,col+c):
+                    accumulator +=1
+        return accumulator
+    
+
+
+    def next_shape(self):
+        new_board = [[False] * self.width for _ in range(self.height)]
+
+        for row in range(self.height):
+            for col in range(self.width):
+                alive_neighbors = self.get_alive_neighbours(row, col)
+                if self.board[row][col]:
+                    if alive_neighbors < 2 or alive_neighbors > 3:
+                        new_board[row][col] = False
+                    else:
+                        new_board[row][col] = True
+                else:
+                    if alive_neighbors == 3:
+                        new_board[row][col] = True
+        self.board = new_board
+
+        
--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 from board import Board
+import time
 board = Board(3,3)
-for x in range(3):
+for x in range(board.height):
      board.place_cell(x,1)
 print(board.as_str())
-
+print(board.get_alive_neighbours(1,2))
+board.next_shape()
+print(board.as_str())
+while True:
+    time.sleep(1)
+    board.next_shape()
+    print(board.as_str())
+     
